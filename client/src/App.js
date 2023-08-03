c6258ad8337e2c3a4d895431992435c28747712d
esrafilelahi <essinho1121@gmail.com>
2023-08-04 00:56:59 +0330

    add react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Cards from "./components/Cards/Cards";
+import Pets from "./components/Pets/Pets";
+import cats from "./mocks/cats.json";
+
+function App() {
+  return (
+    <div>
+      <Pets />
+    </div>
+  );
+}
+
+export default App;
