c4ff57323fb5548aaee0c3366553720fc3fb7722
esrafilelahi <essinho1121@gmail.com>
2023-08-04 16:21:59 +0330

    add card tests

---
+++ client/src/components/Cards/__test__/Card.test.js
@@ -0,0 +1,26 @@
+import { screen, render } from '@testing-library/react';
+import Cards from '../Cards';
+import cats from '../../../mocks/cats.json';
+import { PetsContext } from '../../Pets/Pets';
+
+
+describe("Cards", () => {
+  test("render 5 cards in page", () => {
+    // render PetsContext with values
+    render(
+      <PetsContext.Provider
+        value={{
+          cats: cats,
+          setCats: () => { },
+        }}
+      >
+        <Cards />
+      </PetsContext.Provider>
+    );
+
+    // assertion
+    const cards = screen.getAllByRole("article");
+    expect(cards).toHaveLength(5)
+  })
+
+})
