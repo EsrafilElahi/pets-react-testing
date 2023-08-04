947acc0a2f38b423699e08f70f1fe1565be1794c
esrafilelahi <essinho1121@gmail.com>
2023-08-04 16:11:36 +0330

    add filter tests , unit tests

---
+++ client/src/components/Filter/__test__/Filter.test.js
@@ -0,0 +1,51 @@
+import {screen, render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Filters from '../Filter';
+
+
+describe("Filter", () => {
+  // render
+  beforeEach(() => {
+    render(<Filters filters={{}} setFilters={() => { }} />);
+  }) 
+
+    test("should be able change value of fouvorite select", () => {
+        // find element
+      const favouriteSelect = screen.getByLabelText(/favourite/i);
+      
+      // assertion any
+      expect(favouriteSelect).toHaveValue('any');
+
+      // change favoured
+      userEvent.selectOptions(favouriteSelect, "favoured");
+
+      // assertion favoured
+      expect(favouriteSelect).toHaveValue('favoured');
+      
+      // change not favoured
+      userEvent.selectOptions(favouriteSelect, "not favoured");
+      
+      // assertion not favoured
+      expect(favouriteSelect).toHaveValue('not favoured');
+    });
+
+  test("should be able change value of gender select", () => {
+    // find element
+    const genderSelect = screen.getByLabelText(/gender/i);
+
+    // assertion any
+    expect(genderSelect).toHaveValue('any');
+
+    // change male
+    userEvent.selectOptions(genderSelect, "male");
+
+    // assertion male
+    expect(genderSelect).toHaveValue("male");
+    
+    // change female
+    userEvent.selectOptions(genderSelect, "female");
+
+    // assertion female
+    expect(genderSelect).toHaveValue("female");
+  });
+});
